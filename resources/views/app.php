6b24ba3501b2b42bc4b3689fbd7e8fd5baadca52
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2025-03-17 19:58:40 +0100

    set $isProduction to true in config.php for prod

--- resources/views/app.php
+++ resources/views/app.php
@@ -5,10 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <title>Inertia.js Vanilla PHP Demo</title>
     
-    <?php if (true): /* Always use dev server during development */ ?>
+    <?php
+    // Check if we're in a production environment
+    // Default to production for live server
+    include("../config.php");
+    
+    if (!$isProduction): // Development mode
+    ?>
         <script type="module" src="http://localhost:3000/@vite/client"></script>
         <script type="module" src="http://localhost:3000/resources/js/app.js"></script>
-    <?php else: ?>
+    <?php else: // Production mode ?>
         <link rel="stylesheet" href="/assets/app.css">
         <script type="module" src="/assets/app.js"></script>
     <?php endif; ?>
